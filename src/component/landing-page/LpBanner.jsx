841d54e4931836d2ce847486c8d47d8b88f71579
farizahh <farhanafarizah@gmail.com>
2026-05-28 18:47:28 +0700

    fix: update landing page

--- src/component/landing-page/LpBanner.jsx
+++ src/component/landing-page/LpBanner.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaArrowRightLong } from "react-icons/fa6";
 import BannerImg from '../../assets/img/hero.svg'
+import Img from '../../assets/img/hero.jpg'
 
 const LpBanner = () => {
     return (
@@ -12,17 +13,23 @@ const LpBanner = () => {
                     for your community, all in one place
                 </p>
 
-                <button className="relative z-10 mt-4 lg:mt-8 flex items-center gap-3 bg-[#00AEEF] hover:bg-sky-600 px-6 lg:px-10 py-2 lg:py-4 text-[#FEF7E7] lg:text-lg font-semibold rounded-full">
-                    Get Started
-                    <span className="flex items-center justify-center w-8 h-8 bg-[#FEF7E7] rounded-full">
-                        <FaArrowRightLong className="text-[#00AEEF] lg:text-lg rotate-[-30deg]" />
-                    </span>
-                </button>
+                <div className='flex gap-4'>
+                    <button className="relative z-10 mt-4 lg:mt-8 flex items-center gap-3 border border-[#00AEEF] bg-white hover:bg-[#00AEEF] px-6 lg:px-10 py-2 lg:py-2 text-[#00AEEF] hover:text-white lg:text-lg font-semibold rounded-full">
+                        Explore Portofolio
+                    </button>
+
+                    <button className="relative z-10 mt-4 lg:mt-8 flex items-center gap-3 bg-[#00AEEF] hover:bg-sky-600 px-6 lg:px-10 py-2 lg:py-2 text-[#FEF7E7] lg:text-lg font-semibold rounded-full">
+                        Get Started
+                        <span className="flex items-center justify-center w-8 h-8 bg-[#FEF7E7] rounded-full">
+                            <FaArrowRightLong className="text-[#00AEEF] lg:text-lg rotate-[-30deg]" />
+                        </span>
+                    </button>
+                </div>
             </div>
 
-            <img src={BannerImg} className="w-full -mt-9 lg:-mt-28 relative z-0" />
+            <img src={BannerImg} className="w-full -mt-9 lg:-mt-32 relative z-0" />
 
-            <div className="bg-[#00AEEF] px-10 py-5 text-white flex items-center justify-center">
+            <div className="bg-[#00AEEF] lg:-mt-24 relative px-10 py-5 text-white flex items-center justify-center">
                 <div className="flex items-center gap-32">
 
                     <div className="text-center">
